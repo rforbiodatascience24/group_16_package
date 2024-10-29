4fb5f2403ab97911389bb913a52bfc46914d7d70
EricTorresBT <s243275@dtu.dk>
2024-10-29 09:49:47 +0100

    added data description

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' genetic_code
+#'
+#' This object contains the genetic code, that is, the equivalence between a codon and the resulting amino acid after biological translation.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"genetic_code"
